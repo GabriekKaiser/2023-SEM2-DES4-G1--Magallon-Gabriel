1d4d33e291b34689a4b5d5ce4e55d0870e71bdd2
GabriekKaiser <gabriel.t.13@hotmail.com>
2023-11-13 18:47:16 -0500

    Parcial #3

--- Proyecto#2 Parcial 3/app.js
+++ Proyecto#2 Parcial 3/app.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form');
 const response = document.querySelector('#response');
 const response2 = document.querySelector('#response2');
 // ----------------------------------------
-
+//se crean los numeros aleatorios
 const anyNumber = () => {
   return Math.floor(Math.random() * 98) + 1;
 }
@@ -10,33 +10,34 @@ const anyNumber = () => {
 function isValid(quantity) {
   return quantity < 1 || quantity > 50;
 }
-// Function de crear cuadros
+// Function de crear cuadros y introducir numeros aleatorios
 function createCards(quantity) {
   let cards = '';
-  let storedNumbers = [];
+  saveNumbers = []; 
   for (let i = 1; i <= quantity; i++) {
     cards += `<div>`;
     for (let j = 1; j <= i; j++) {
       const randomNumber = anyNumber();
       cards += `<div class="square">${randomNumber}</div>`;
-      //Store numbers se estan almacenando los numeros lo puedes ver en el console log
-      storedNumbers.push(randomNumber);
+      saveNumbers.push(randomNumber); 
     }
     cards += `</div>`;
   }
-  console.log(storedNumbers);
-
   return cards;
 }
-//Aqui Dberian llegar lo valores del randomNumber para imprimirlos en el #Response2
-function getRandomNumbers(quantity){
+
+//aqui llegan los numeros aleatorios para que solo haya 1 que esos se sumen
+function getRandomNumbers(saveNumbers){
+  const onlyOneValue = new Set(saveNumbers);
   let cards2 = '';
-  for(let i = 1; i <= quantity; i++){
-    for(let j = 1; j <= i; j++){
-      const randomNumber = anyNumber();
-      cards2 += `<div class="square2">${randomNumber}</div>`;
-    }
+  const onlyOneArray = [...onlyOneValue];
+  for(let i = 0; i < onlyOneArray.length; i++){
+    cards2 += `<div class="square2">${onlyOneArray[i]}</div>`;
   }
+
+  let total = onlyOneArray.reduce((acc, number) => acc + number, 0);
+  cards2 += `<div class="result">${total}</div>`;
+
 return cards2;
 }
 
@@ -63,7 +64,7 @@ function onFormSubmit(event) {
   const cards = createCards(quantity);
   drawCards(cards);
 
-  const cards2 = getRandomNumbers(quantity);
+  const cards2 = getRandomNumbers(saveNumbers);
   drawCards2(cards2)
 }
 
