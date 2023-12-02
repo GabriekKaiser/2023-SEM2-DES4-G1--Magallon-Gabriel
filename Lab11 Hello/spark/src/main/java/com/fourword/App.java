4a04c0c33ab72a76023b2f3f8a5b874b69b97fa4
GabriekKaiser <gabriel.t.13@hotmail.com>
2023-12-02 16:32:48 -0500

    Laboratorio #11 - Hello world en Java Spark

---
+++ Lab11 Hello/spark/src/main/java/com/fourword/App.java
@@ -0,0 +1,13 @@
+package com.fourword;
+
+/**
+ * Hello world!
+ */
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World - Spark");
+        System.out.println("Hello World");
+    }
+}
